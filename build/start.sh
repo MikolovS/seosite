7a247a9dba6c770fadc80d822dc24cda7d05dd20
samoletik <sergeymilov@gmail.com>
2018-07-13 09:06:56 +0300

    start

---
+++ build/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+. $(dirname "$0")/help.sh
+
+path=$(current_path)
+project_dir="$path/../"
+
+if $ENV_DEV; then
+    cd "$path/dev" \
+    && docker-compose -p $PROJECT_NAME_DEV up -d
+
+    CONTAINER=$(docker ps -aqf "name=${PROJECT_NAME_DEV}_app")
+
+    docker exec $CONTAINER chmod 777 -R storage/ \
+    && docker exec $CONTAINER php artisan config:clear \
+    && docker exec $CONTAINER php artisan key:generate \
+    && docker exec $CONTAINER php artisan migrate
+fi
